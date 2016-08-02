9b5a05f0cdc82f604664b29d8c8b534689a8cab6
Nikhil Mehta <nikhilmht673@gmail.com>
2016-08-02 17:01:15 +0530

    Some Tweaks

--- app/src/main/java/com/algofocus/barcodescanzxing/SplashScreen.java
+++ app/src/main/java/com/algofocus/barcodescanzxing/SplashScreen.java
@@ -2,6 +2,7 @@ package com.algofocus.barcodescanzxing;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,13 +11,17 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_splash_screen);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
               Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
+                finish();
             }
         },5000);
+
     }
 }
